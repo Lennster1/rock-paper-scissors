42954d4839e066e9f5d8533dfbc1155878fd83e9
Lenny Luo <76703996+Lennster1@users.noreply.github.com>
2024-02-21 21:45:47 -0500

    add win/lose/tie logic

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3)
-    switch (randomNumber) {
-      case 0:
-        return 'rock'
-      case 1:
-        return 'paper'
-      case 2:
-        return 'scissors'
-    }
+  let randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
   }
+}
 
-console.log(getComputerChoice());
+console.log(`Computer choice: ${getComputerChoice()}`);
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    return "You Tied! Try again.";
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    return "You Lost! Paper beats Rock.";
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return "You Win! Rock beats Paper!.";
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    return "You Win! Rock beats Scissors!.";
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    return "You Lost! Scissors beats Paper.";
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    return "You Win! Scissors beats Paper!.";
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    return "You Lost! Rock beats Scissors.";
+}
+}
+
+const playerSelection = "RocK".toLocaleLowerCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
